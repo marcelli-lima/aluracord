c65eb0a8cf9128dcc64c7012f3ef2fa63c584912
marcelli-lima <marcelli_lima@hotmail.com>
2022-01-29 13:01:47 -0300

    submit das informaçoes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9c54538921fbaade"; }
+/******/ 		__webpack_require__.h = function() { return "6a147435d02e1a68"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
